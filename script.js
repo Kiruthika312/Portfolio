0ca4299f201710cc26311bcbc4070dc25e29b402
Kiruthika312 <akiruthika61@gmail.com>
2024-09-30 19:14:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidebar=document.getElementById("sidebar")
+
+function openNavbar(){
+    sidebar.style.right="0"
+    sidebar.style.display="block"
+
+}
+function closeNavbar(){
+    sidebar.style.right="-60%"
+    sidebar.style.display="none"
+}
